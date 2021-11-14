dd8f00c483a8c6ade123d338979ed288e06223ef
Aswini Sankar <53120389+AswiniSankar@users.noreply.github.com>
2021-11-14 10:54:57 +0530

    Add files via upload

---
+++ audiocode.py
@@ -0,0 +1,16 @@
+import pyttsx3 
+import PyPDF2
+
+book = open('/home/aswini/Projects/Python/oscarstory.pdf', 'rb') #pdf to read
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages #get the page count of pdf
+#print(pages)
+page = pdfReader.getPage(3) #get starting page of the pdf
+speaker = pyttsx3.init() 
+speaker.setProperty("rate", 120)
+speaker.setProperty('volume', 0.4)
+for num in range(3, pages): #reading each page
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()  
+
